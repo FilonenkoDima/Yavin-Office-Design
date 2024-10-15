01a93de983ba9dc4abd2b9bf6e7736474ed14fa0
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2024-10-15 22:09:17 +0300

    added stats increment

--- js/script.js
+++ js/script.js
@@ -10,4 +10,30 @@ function userScroll() {
   });
 }
 
-document.addEventListener("DOMContentLoaded", userScroll);
+function incrementStats() {
+  const counters = document.querySelectorAll(".counter");
+
+  counters.forEach((counter) => {
+    counter.innerText = 0;
+
+    const updateCounter = () => {
+      const target = +counter.getAttribute("data-target");
+      const c = +counter.innerText;
+
+      const increment = target / 200;
+
+      if (c < target) {
+        counter.innerText = Math.ceil(c + increment);
+        setTimeout(updateCounter, 1);
+      } else {
+        counter.innerText = target;
+      }
+    };
+
+    updateCounter();
+  });
+}
+
+[userScroll, incrementStats].forEach((fn) =>
+  document.addEventListener("DOMContentLoaded", fn)
+);
