6f19295b07a0d5bc730154eafb952bc1e30d7df8
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2024-10-16 09:55:06 +0300

    added 'scroll to top' button

--- js/script.js
+++ js/script.js
@@ -1,13 +1,21 @@
 function userScroll() {
   const navbar = document.querySelector(".navbar");
+  const toTopBtn = document.querySelector("#to-top");
 
   window.addEventListener("scroll", () => {
     if (window.scrollY > 50) {
       navbar.classList.add("navbar-sticky");
+      toTopBtn.classList.add("show");
     } else {
       navbar.classList.remove("navbar-sticky");
+      toTopBtn.classList.remove("show");
     }
   });
+}
+
+function scrollToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
 
 function incrementStats() {
@@ -37,3 +45,4 @@ function incrementStats() {
 [userScroll, incrementStats].forEach((fn) =>
   document.addEventListener("DOMContentLoaded", fn)
 );
+document.querySelector("#to-top").addEventListener("click", scrollToTop);
